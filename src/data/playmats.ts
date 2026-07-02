2d0ffbf7808894cab78b8fcf8c26815dc51dc8f6
TCGDragonBall <tcgdragonball@gmail.com>
2026-07-02 13:09:38 +0200

    Playmats

--- src/data/playmats.ts
+++ src/data/playmats.ts
@@ -73,4 +73,48 @@ PM-CEL19-14	Celebrations 2019 Playmat 14	Accessories	Playmat	Neutral	PM_CELEBRAT
 PM-CEL19-15	Celebrations 2019 Playmat 15	Accessories	Playmat	Neutral	PM_CELEBRATIONS_2019	-	-	-	-	-	-	-	-
 PM-CEL19-16	Celebrations 2019 Playmat 16	Accessories	Playmat	Neutral	PM_CELEBRATIONS_2019	-	-	-	-	-	-	-	-
 PM-CEL19-17	Celebrations 2019 Playmat 17	Accessories	Playmat	Neutral	PM_CELEBRATIONS_2019	-	-	-	-	-	-	-	-
+PM-UWS2-01	2 Player Playmat UWS2 Pre Release Tournament	Accessories	Playmat	Neutral	PM_PRE_RELEASE_UWS2	-	-	-	-	-	-	-	-
+PM-PTO20-01	Participation Playmat Premier TO Online Event Series 2020	Accessories	Playmat	Neutral	PM_PREMIER_TO_2020	-	-	-	-	-	-	-	-
+PM-PTO20-02	Winner Playmat Premier TO Online Event Series 2020	Accessories	Playmat	Neutral	PM_PREMIER_TO_2020	-	-	-	-	-	-	-	-
+PM-CM20-01	Playmat Regionals (Winner) Championship 2020	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2020	-	-	-	-	-	-	-	-
+PM-CM20-02	Playmat Team Wars (Champion) Championship 2020	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2020	-	-	-	-	-	-	-	-
+PM-CM20-03	Playmat Draft Battles (Champion) Championship 2020	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2020	-	-	-	-	-	-	-	-
+PM-CM20-04	Playmat Participation A Championship 2020	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2020	-	-	-	-	-	-	-	-
+PM-CM20-05	Playmat Participation B Championship 2020	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2020	-	-	-	-	-	-	-	-
+PM-OCC21-01	Exclusive Event Playmat Online Champions Cup 2021	Accessories	Playmat	Neutral	PM_ONLINE_CHAMPIONS_CUP_2021	-	-	-	-	-	-	-	-
+PM-CM21-01	Playmat (Side Event Winner) Championship 2021	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2021	-	-	-	-	-	-	-	-
+PM-CM21-02	Playmat (Winner) Championship 2021	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2021	-	-	-	-	-	-	-	-
+PM-CM21-03	Playmat 1st Place Championship 2021	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2021	-	-	-	-	-	-	-	-
+PM-CM21-04	Playmat 2nd Place Championship 2021	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2021	-	-	-	-	-	-	-	-
+PM-CM21-05	Playmat 3rd Place Championship 2021	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2021	-	-	-	-	-	-	-	-
+PM-CM21-06	Playmat Top 32 Championship 2021	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2021	-	-	-	-	-	-	-	-
+PM-CM21-07	Playmat Participation Championship 2021	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2021	-	-	-	-	-	-	-	-
+PM-CM21-08	Playmat Judge Championship 2021	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2021	-	-	-	-	-	-	-	-
+PM-CM21-09	Playmat Championship 2021	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2021	-	-	-	-	-	-	-	-
+PM-WC21-01	2021 World Championship Playmat	Accessories	Playmat	Neutral	PM_WORLD_CHAMPIONSHIP_2021	-	-	-	-	-	-	-	-
+PM-FEST-01	UW Cup Winner Playmat DBSCG Fest	Accessories	Playmat	Neutral	PM_DBSCG_FEST	-	-	-	-	-	-	-	-
+PM-CM22-01	Playmat (Top 16 / Top Cut) Championship 2022 Regionals	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2022_REGIONALS	-	-	-	-	-	-	-	-
+PM-CM22-02	Playmat (Winner) Championship 2022 Regionals	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2022_REGIONALS	-	-	-	-	-	-	-	-
+PM-CM22-03	Playmat Champion Championship 2022 Finals	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2022_FINALS	-	-	-	-	-	-	-	-
+PM-CM22-04	Playmat 2nd Place Championship 2022 Finals	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2022_FINALS	-	-	-	-	-	-	-	-
+PM-CM22-05	Playmat 3rd Place Championship 2022 Finals	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2022_FINALS	-	-	-	-	-	-	-	-
+PM-CM22-06	Playmat Top 32 Championship 2022 Finals	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2022_FINALS	-	-	-	-	-	-	-	-
+PM-CM22-07	Playmat Participation Championship 2022 Finals	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2022_FINALS	-	-	-	-	-	-	-	-
+PM-CM22-08	Playmat Judge Championship 2022 Finals	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2022_FINALS	-	-	-	-	-	-	-	-
+PM-WC22-01	Playmat (Top 4) 2022 World Championship	Accessories	Playmat	Neutral	PM_WORLD_CHAMPIONSHIP_2022	-	-	-	-	-	-	-	-
+PM-CM23-01	Playmat (Winner) Championship 2023 Online/Offline Regionals (March-May)	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2023_REGIONALS	-	-	-	-	-	-	-	-
+PM-CM23-02	Playmat (Winner) Championship 2023 Online/Offline Regionals (June-August)	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2023_REGIONALS	-	-	-	-	-	-	-	-
+PM-CM23-03	Playmat (Winner) Championship 2023 Online/Offline Regionals (September-November)	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2023_REGIONALS	-	-	-	-	-	-	-	-
+PM-CM23-04	Playmat (1st place) Championship 2023 Final Championship	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2023_FINALS	-	-	-	-	-	-	-	-
+PM-CM23-05	Playmat (2nd place) Championship 2023 Final Championship	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2023_FINALS	-	-	-	-	-	-	-	-
+PM-CM23-06	Playmat (3rd place) Championship 2023 Final Championship	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2023_FINALS	-	-	-	-	-	-	-	-
+PM-CM23-07	Playmat (top 32) Championship 2023 Final Championship	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2023_FINALS	-	-	-	-	-	-	-	-
+PM-CM23-08	Playmat (participation) Championship 2023 Final Championship	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2023_FINALS	-	-	-	-	-	-	-	-
+PM-CM23-09	Playmat (Judge) Championship 2023 Final Championship	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2023_FINALS	-	-	-	-	-	-	-	-
+PM-WC23-01	Playmat Championship 2023 World Championship	Accessories	Playmat	Neutral	PM_WORLD_CHAMPIONSHIP_2023	-	-	-	-	-	-	-	-
+PM-CM24-01	Winner Playmat (Champion) Championship 2024 Online/Offline Regionals Wave 1	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2024_REGIONALS	-	-	-	-	-	-	-	-
+PM-WC24-01	Playmat (participation) World Championship 2024	Accessories	Playmat	Neutral	PM_WORLD_CHAMPIONSHIP_2024	-	-	-	-	-	-	-	-
+PM-CM25-01	Playmat Championship 2025 Online/Offline Regionals Wave 1	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2025_REGIONALS	-	-	-	-	-	-	-	-
+PM-CM25-02	Playmat Championship 2025 Online/Offline Regionals Wave 2	Accessories	Playmat	Neutral	PM_CHAMPIONSHIP_2025_REGIONALS	-	-	-	-	-	-	-	-
+PM-WC25-01	Playmat World Championship 2025	Accessories	Playmat	Neutral	PM_WORLD_CHAMPIONSHIP_2025	-	-	-	-	-	-	-	-
 `;
